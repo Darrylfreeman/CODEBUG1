b4abce8460782ae4044ace98aced0e1d58cd5768
Darrylfreeman <william.darryl34@gmail.com>
2024-08-29 11:47:30 -0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,17 @@
+name="Georgia" #varibale storing name 
+age="3.5"
+hair_color="brown"
+
+print (name)
+
+def hair_color_function():
+    if hair_color == "yellow":
+      print("You've got yellow")
+    else:
+      print("You dont have yellow hair")
+      
+hair_color_function()
+    
+sentenceemerge = name + "  " + "is" + " " +age
+print (sentenceemerge)
+    
